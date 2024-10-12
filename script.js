8f1ee3e7cceb93bd3f481936cfb3b4534df8248f
Mohammed Hadi Alizade <mohammed.hadi.alizade@student.tabyenskilda.se>
2024-10-12 06:39:17 +0200

    Portfolio - Potential preloader animation?

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+
+setTimeout(() => {
+    const loader = document.querySelector(".loader");
+    
+    // Disable scrolling
+    document.body.style.overflow = "hidden";
+    
+    loader.classList.add("loader--hidden");
+    
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+      
+      // Re-enable scrolling
+      document.body.style.overflow = "auto";
+    });
+  }, 4000);
+
+  */
